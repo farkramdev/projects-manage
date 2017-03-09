fdb7be9d3d28cda3310df58cf1d91f38c8648331
Farkram <worrawoot@addlink.com>
2017-03-09 17:33:14 +0700

    Update Frontend

--- Projects-store/src/app/app.module.ts
+++ Projects-store/src/app/app.module.ts
@@ -1,20 +1,28 @@
+import { RequireAuth, RequireSignin } from './shared/services/authentication.service';
+import { HttpService } from './shared/services/http.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    HttpService,
+    RequireAuth,
+    RequireSignin
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
